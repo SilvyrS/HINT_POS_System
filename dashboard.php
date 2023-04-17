ef7360f0df9fbc7674852c6df51e0773ffa7927d
TracyMarieAndrea <tracymarieandrea.madresta@wvsu.edu.ph>
2023-04-17 22:11:12 +0800

    first commit

---
+++ dashboard.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/dashboard_style.css">
+
+    <title>HINT POS System | HOME SCREEN</title>
+</head>
+
+<body>
+    <header>
+        <img src="" alt="logo" class="logo">
+        <nav>
+            <ul class="nav_links">
+                <li><a href="#">Home Screen</a></li>
+                <li><a href="#">Home Screen</a></li>
+                <li><a href="#">Home Screen</a></li>
+                <li><a href="#">Home Screen</a></li>
+            </ul>
+            <a href="main.php"><button>Logout</button></a>
+        </nav>
+    </header>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/index_style.css">
+
+    <title>HINT POS | LOGIN</title>
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <div class="container">
+            <div class="logo">
+                <img src="assets/logo.png" alt="HINT POS Logo">
+            </div>
+            <h2 class="title-login">LOGIN</h2>
+            <form action="dashboard.php" method="post">
+                <div class="username">
+                    <input type="text" name="username" id="userName" placeholder="Username">
+                </div>
+                <div class="password">
+                    <input type="password" name="password" id="pwd" placeholder="Password">
+                </div>
+                <button name="submit" type="submit" class="login_btn">Login</button>
+            </form>
+            <div class="link">
+                <a href="#">Forget password?</a> or <a href="#">Sign up</a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
